1086e32193c407e114c56e9749a54e8d7836230f
ww86 <85488277+ww86@users.noreply.github.com>
2025-05-10 04:33:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function hexToRgba(hex, opacity) {
 
 // --- Utility: Draw a rounded rectangle path.
 function roundRect(ctx, x, y, width, height, radius) {
-  let r1 = radius;
+  let r1 = 0;
   ctx.beginPath();
   ctx.moveTo(x + r1, y);
   ctx.lineTo(x + width - r1, y);

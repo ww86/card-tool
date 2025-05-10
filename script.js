795c7e7ab03e80addc2608e0f659572f9b9491b2
ww86 <85488277+ww86@users.noreply.github.com>
2025-05-10 05:30:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ document.addEventListener("DOMContentLoaded", function () {
     ctx.save();
     ctx.lineWidth = margin * 2;
     ctx.strokeStyle = "black";
-    roundRect(ctx, innerX, innerY, innerWidth, innerHeight, margin);
+    roundRect(ctx, innerX, innerY, innerWidth - margin, innerHeight - margin, margin);
     ctx.stroke();
     ctx.restore();
 
